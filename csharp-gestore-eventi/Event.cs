9ec613d811a15feb86d444bb15c43cecced92ce1
Hirisci <www.alan92@icloud.com>
2022-09-26 10:53:31 +0200

    refatctoring milestone 1

--- csharp-gestore-eventi/Event.cs
+++ csharp-gestore-eventi/Event.cs
@@ -9,33 +9,62 @@ namespace csharp_gestore_eventi
 {
     internal class Event
     {
+        private string? _title;
+        private DateTime _date;
+        private int _seatsMax;
+        private int _reservedSeats;
         
-        public string Title
+        public string? Title
         {
-            get => Title;
+            get => _title;
             set
             {
-                if (value == "") throw new Exception("Impossibile impostare vuoto il campo titolo evento");
-                else Title = value;
+                if (value == "" || value == null) { 
+                throw new Exception("Impossibile impostare vuoto il campo titolo evento");
+                }
+                _title = value;
             }
         }  
         public DateTime Date { 
-            get=> Date; 
+            get=> _date; 
             set {
-                if (DateTime.Now > value) throw new Exception("Impossibile impostare la data");
-                else Date = value;
+                if (DateTime.Now > value) { 
+                throw new Exception("Impossibile impostare la data");
+                }
+
+                _date = value;
             }
-        } 
-        
+        }
         public int SeatsMax { 
-            get => SeatsMax;  
+            get => _seatsMax;  
             init {
-                if (value <= 0) throw new Exception("Impossibile inserire un numero negativo");
-                else SeatsMax = value;
+                if (value <= 0) { 
+                    throw new Exception("Impossibile inserire un numero negativo");
+                } 
+                
+                _seatsMax = value;
             } 
         }
-        public int ReservedSeats { get; private set; }
+        public int ReservedSeats { 
+            get=> _reservedSeats;
+            private set {
+                if (_date < DateTime.Now) { 
+                    throw new Exception("Operazione impossibile, l'evento é gia passato");
+                } 
+                if (_reservedSeats + value > _seatsMax){
+                    throw new Exception($"Impossibile eseguire l'operazione, Sono disponibili solo {_seatsMax - _reservedSeats} posti ");
+                }
+                if (_reservedSeats - value <= 0) { 
+                    throw new Exception($"Impossibile eseguire l'operazione, Posti riservati sono solo {_reservedSeats}");
+                }
 
+                _reservedSeats = value;
+
+            } 
+        }
+        
+        
+        //public int EmptySeats { get=>EmptySeats; private set=> EmptySeats=SeatsMax-ReservedSeats; }
         public Event(string title, DateTime date, int seatsMax)
         {
             Title = title;
@@ -47,22 +76,12 @@ namespace csharp_gestore_eventi
         //prenota posti
         public void BookSeats(int seats) 
         {
-             
-            if (Date< DateTime.Now) throw new Exception("Impossibile riservare i posti, l'evento é gia passato");
-
-            if (ReservedSeats + seats > ReservedSeats) throw new Exception($"Impossibile eseguire l'operazione, Sono disponibili solo {SeatsMax - ReservedSeats} posti ");
-
             ReservedSeats += seats;
-
         }
 
         //disdici posti
         public void CancelBookSeats(int seats)
         {
-            if (Date < DateTime.Now) throw new Exception("Impossibile cancellare i posti riservati, l'evento é gia passato");
-
-            if (ReservedSeats - seats <= 0) throw new Exception("Operazione non riuscita, I posti da disdire non sono sufficenti");
-
             ReservedSeats -= seats; 
         }
 
