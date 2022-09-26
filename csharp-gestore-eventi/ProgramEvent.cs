2fea274d7b5b3be4c1c2f660dfdabbcd509ef7db
Hirisci <www.alan92@icloud.com>
2022-09-26 12:18:42 +0200

    Milestone 3 completata

---
+++ csharp-gestore-eventi/ProgramEvent.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    public class ProgramEvent
+    {
+        private List<Event> _events;
+        public string Title { get; set; }
+        public int Count { get { return _events.Count; } }
+        public ReadOnlyCollection<Event> Events { get; }
+
+        public ProgramEvent(List<Event> events, string title)
+        {
+            _events = events;
+            Title = title;
+            Events = _events.AsReadOnly();
+        }
+
+        public void AddEvent(Event newEvent){
+            _events.Add(newEvent);
+        }
+        public void ClearList() { 
+            _events.Clear();
+        } 
+
+        public List<Event> EventsAvailable(DateTime date)
+        {
+            return _events.FindAll(vnt => vnt.Date > DateTime.Now);
+        }
+
+        public string PrintTour()
+        {
+            return $"{Title} /n" + PrintEvent(_events) ;
+        }
+
+        static string PrintEvent(List<Event> ListEvent)
+        {
+            string str = "";
+            foreach(Event e in ListEvent)
+            {
+                str += e.ToString()+"/n";
+            }
+            if(str.Length > 0){
+                return str;
+            }
+
+            throw new Exception("Nessun evente presente nel Tour");
+        }
+
+
+        
+    }
+}
